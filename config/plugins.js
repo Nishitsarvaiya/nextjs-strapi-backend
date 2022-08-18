f218964c604df6bd6d9b6bc6c5318b73e9d58e90
Nishit-Sarvaiya <nishit@darwindigital.com>
2022-08-18 17:38:32 +0530

    added ckeditor

--- config/plugins.js
+++ config/plugins.js
@@ -14,4 +14,83 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  ckeditor: {
+    enabled: true,
+    config: {
+      plugin: {
+        strapiTheme: true,
+      },
+      editor: {
+        toolbar: {
+          items: [
+            "heading",
+            "|",
+            "bold",
+            "italic",
+            "underline",
+            "|",
+            "bulletedList",
+            "numberedList",
+            "|",
+            "StrapiMediaLib",
+            "blockQuote",
+            "mediaEmbed",
+            "link",
+            "|",
+            "htmlEmbed",
+            "code",
+            "codeBlock",
+            "|",
+            "subscript",
+            "superscript",
+            "|",
+            "fullScreen",
+            "undo",
+            "redo",
+            "|",
+            "sourceEditing",
+          ],
+        },
+        heading: {
+          options: [
+            {
+              model: "paragraph",
+              title: "Paragraph",
+              class: "ck-heading_paragraph",
+            },
+            {
+              model: "heading1",
+              view: "h1",
+              title: "Heading 1",
+              class: "ck-heading_heading1",
+            },
+            {
+              model: "heading2",
+              view: "h2",
+              title: "Heading 2",
+              class: "ck-heading_heading2",
+            },
+            {
+              model: "heading3",
+              view: "h3",
+              title: "Heading 3",
+              class: "ck-heading_heading3",
+            },
+            {
+              model: "heading4",
+              view: "h4",
+              title: "Heading 4",
+              class: "ck-heading_heading4",
+            },
+            {
+              model: "heading5",
+              view: "h5",
+              title: "Heading 5",
+              class: "ck-heading_heading5",
+            },
+          ],
+        },
+      },
+    },
+  },
 });
